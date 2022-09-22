41d8dc845a7f5bad5251df04b9d1d35938a86ede
MagnumOpus <mlindholm20@gmail.com>
2022-09-22 16:24:17 +0200

    2. commit

--- Specification.md
+++ Specification.md
@@ -1,8 +1,9 @@
 - Der er en ledger, der holder styr på accounts. HVer Peer har en kopi af denne ledger
 - Hver account har en balance (Så der er et Map fra String -> int)
-- Alle peers i netværket har den samme Ledger
+- Alle peers i netværket har på et tidspunkt den samme Ledger
 - Der kan laves en transaktion på netværket.
 - En transaktion består af en amount (int), en From og To (String)
 - Amount bliver overført fra From -> To
-- Når en peer lave en transaktion, opdaterer den de sin ledger. 
+- Når en peer lave en transaktion, opdaterer den sin ledger. 
+- efter en opdatering a ledgeren, så floodes netværket med den nye ledger
 - Alle peers har på et tidspunkt den samme ledger.
--- peer.go
+++ peer.go
@@ -1,27 +1,66 @@
 package peer
 
 import (
-	"peer/account"
+	"account"
+	"encoding/gob"
+	"net"
 )
 
 type Peer struct {
-	ledger account.Ledger
-	peers  list[Peer]
-	ip     int
-	addr   string
+	ledger      account.Ledger
+	connections []Connection
 }
 
-func FloodMessage(msg *account.Ledger) *Peer {
-	for i, e := range peers {
-		ch := make(chan<- account.Ledger)
-		ch <- msg
+func (p Peer) UpdateLedger(tx *account.Transaction) {
+	//Need to do some checks, but what?
+	ledger := *p.ledger
+	ledger.Transact(tx)
+}
+
+func (p Peer) FloodMessage(msg *nt.Ledger) {
+	encodedmsg := encoder
+	for i, conn := range p.connections {
+		conn, err := net.Dial("tcp", conn.Address.String())
 	}
 }
 
-func FloodTransaction(tx *Transaction) *Peer {
-
+func (p Peer) FloodTransaction(tx *Transaction) {
+	for i, e := range p.connections {
+		conn, err := net.Dial(tcp, e.address)
+		encoder := gob.NewEncoder(conn)
+		encoder.Encode(msg)
+	}
 }
 
-func Recive(c *chan account.Ledger) *Peer {
-	ledger := <-c
+func (p Peer) Listen(c *chan account.Ledger) {
+	l, _ = net.Listen("tcp", Peer.address)
+	for {
+		conn, eer := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go HandleConnection(conn)
+	}
+}
+
+func (p Peer) Init() {
+	p.TryToConnect()
+	p.Listen()
+}
+
+func (p Peer) HandleConnection(conn net.conn) {
+	//Open Previous Conneciton
+}
+
+func (p Peer) TryToConnect() {
+	for i, conn := range p.connections {
+		go HandleConnection(conn.Conneciton)
+	}
+}
+
+type Connection struct {
+	Connection net.conn
+	Address    string
+	Decoder    gob.decoder
+	Encoder    gob.encoder
 }
