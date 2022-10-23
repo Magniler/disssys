8cfa8e018b04ac6c552cbea2108c024f156558f6
Magnus Lindholm Nielsen <malini@aarhus.dk>
2022-10-23 09:28:41 +0200

    Draft: Mistake in the connections methods

--- src/peer/peer.go
+++ src/peer/peer.go
@@ -12,19 +12,19 @@ import (
 )
 
 type Peer struct {
-	address     string
-	ledger      *account.Ledger
-	connections []connection.Connection
+	Address     string
+	Ledger      *account.Ledger
+	Connections []connection.Connection
 }
 
 func (p Peer) UpdateLedger(tx *account.Transaction) {
 	//Need to do some checks, but what?
-	ledger := p.ledger
+	ledger := p.Ledger
 	ledger.Transact(tx)
 }
 
 func (p Peer) FloodTransaction(tx *account.Transaction) {
-	for _, c := range p.connections {
+	for _, c := range p.Connections {
 		conn := c.Connection
 		encoder := gob.NewEncoder(*conn)
 		encoder.Encode(tx)
@@ -38,7 +38,7 @@ func (p Peer) SendMessage(msgType string, connection connection.Connection) {
 		connection.Encoder.Encode(msg)
 		return
 	case "Ask for connections":
-		for _, c := range p.connections {
+		for _, c := range p.Connections {
 			msg := msgType + ":" + c.Address
 			err := c.Encoder.Encode(msg)
 			if err != nil {
@@ -50,14 +50,19 @@ func (p Peer) SendMessage(msgType string, connection connection.Connection) {
 }
 
 func (p Peer) Listen() {
-	l, _ := net.Listen("tcp", p.address)
-	defer l.Close()
+	l, err := net.Listen("tcp", p.Address)
+	if err != nil {
+		defer l.Close()
+	}
+	if err != nil {
+		panic(0)
+	}
+	println("Now listening on " + l.Addr().String())
 	for {
-		fmt.Println("Now listening on " + p.address)
 		conn, _ := l.Accept()
+		println("Got at connection from ", conn.RemoteAddr().String())
 		go p.HandleConnection(conn)
 	}
-
 }
 
 // If the peer is  unable to connect to the network for any reason
@@ -66,46 +71,46 @@ func (p Peer) MakeOwnNetwork() {
 	name, _ := os.Hostname()
 	ip, _ := net.LookupHost(name)
 	// We need to join the ip to make into one string
-	hostPort := ip[0] + ":" + "1616"
-	// :0 os suppossed to be an ephereal port, which means the os
-	// finds the next avaivable port
+	hostPort := ip[0] + ":" + "16160"
+	// :16160 is chosen at random
+	// TODO: Make it soo the port isn't hardcoded
 	addr, err := net.ResolveTCPAddr("tcp", hostPort)
 	if err != nil {
-		panic(-1)
-	}
-	conn, err := net.Dial("tcp", addr.String())
-	if err != nil {
+		fmt.Println(err)
 		panic(-1)
 	}
 	ownConnection := connection.Connection{
-		Connection: &conn,
+		Connection: nil,
 		Address:    addr.String(),
-		Decoder:    gob.NewDecoder(conn),
-		Encoder:    gob.NewEncoder(conn),
+		Decoder:    nil,
+		Encoder:    nil,
 	}
-	p.connections = append(p.connections, ownConnection)
+	p.Connections = append(p.Connections, ownConnection)
 	p.Listen()
 }
 
 // Peer tries to connect to the network on addr:port.
 // If it fails it initializes its own network
 func (p Peer) Connect(addr string, port int) {
+	p.Connections = make([]connection.Connection, 0)
 	fullAddr := addr + ":" + strconv.Itoa(port)
 	c, err := net.Dial("tcp", fullAddr)
 	if err != nil {
-		fmt.Println("Failed to establish connection, initialising own network")
+		fmt.Println("Failed to establish connection, initializing own network")
 		p.MakeOwnNetwork()
 		return
 	}
 	newConnection := &connection.Connection{
 		Connection: &c,
-		Address:    fullAddr,
+		Address:    c.LocalAddr().String(),
 		Decoder:    gob.NewDecoder(c),
 		Encoder:    gob.NewEncoder(c),
 	}
+	println(c.LocalAddr().String())
 	// TODO: Right now it does not add its own conneciton to its lis of
 	// connections
-	p.connections = append(p.connections, *newConnection)
+	p.Connections = append(p.Connections, *newConnection)
+}
 
 type String struct {
 	Msgfmt string
@@ -131,12 +136,11 @@ func (p Peer) HandleConnection(conn net.Conn) {
 			Decoder:    decoder,
 			Encoder:    gob.NewEncoder(conn),
 		}
-		p.connections = append(p.connections, *newConnection)
+		p.Connections = append(p.Connections, *newConnection)
 		return
 	case "Transaction":
 		return
 	case "Ask for connections":
 		return
-		}
 	}
 }
--- src/peer/peer_test.go
+++ src/peer/peer_test.go
@@ -1,12 +1,28 @@
 package peer_test
 
 import (
+	"net"
+	"os"
 	"peer"
+	"testing"
+	"time"
 )
 
-func ExamplePeer_Connect() {
+func ExamplePeer_MakeOwnNetwork() {
 	p := peer.Peer{}
-	p.Connect("Giberish", 42)
-	// Output: Failed to establish connection, initialising own network
-	//FIXME: Throws exception, probaply an permissions proble, let andreas see try it.
+	go p.Connect("Giberish", 42)
+	time.Sleep(5 * time.Second)
+	//Output: Failed to establish connection, initializing own network
+}
+
+func TestPeer_Connect(t *testing.T) {
+	p1 := peer.Peer{}
+	go p1.Connect("Giberish", 42)
+	// This peer then initializes its own network on 192.168.1.152:16160
+	time.Sleep(5 * time.Second)
+	p2 := peer.Peer{}
+	osname, _ := os.Hostname()
+	ip, _ := net.LookupHost(osname)
+	go p2.Connect(ip[0], 16160)
+	println(p2.Connections)
 }
